13dab5a7c0c8aa2b199671d9d764b6949bc3e266
tamyfabius <tamyfabius@gmail.com>
2017-05-27 19:52:10 +0200

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
